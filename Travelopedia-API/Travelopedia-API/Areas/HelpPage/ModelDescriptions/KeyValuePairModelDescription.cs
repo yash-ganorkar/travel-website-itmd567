29ad58c128cb30c28a52617224fb2e6aa5b80b86
yash-ganorkar <yash.ganorkar@outlook.com>
2017-09-17 00:38:11 -0500

    Change #9: Adding API part of the project to the main project.
    Change #10: Added sample code for testing purpose.

---
+++ Travelopedia-API/Travelopedia-API/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace Travelopedia_API.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ Travelopedia-API/Travelopedia-API/Controllers/UserLoginController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Travelopedia_API.Models;
+
+namespace Travelopedia_API.Controllers
+{
+    public class UserLoginController : ApiController
+    {
+        UserCredentials[] usercredentials = new UserCredentials[]
+        {
+            new UserCredentials { username="yash", password="yash" },
+            new UserCredentials { username="shruti", password="shruti" }
+        };
+
+        [HttpGet]
+        [ActionName("Login")]
+        
+        public IHttpActionResult CheckValidLogin(string username)
+        {
+            var validUser = usercredentials.FirstOrDefault((u) => u.username == username);
+
+            if(validUser == null)
+            {
+                return NotFound();
+            }
+            return Ok(validUser);
+        }
+    }
+}
