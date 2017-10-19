8396d966f0c5eecb5f2a835a432d024e0b3d8568
yash-ganorkar <yash.ganorkar@outlook.com>
2017-10-18 22:59:35 -0500

    Change #17:  Added Cars and Hotel quote facility

--- Travelopedia/Travelopedia/Results.aspx.cs
+++ Travelopedia/Travelopedia/Results.aspx.cs
@@ -10,41 +10,75 @@ using Google.Apis.QPXExpress.v1;
 using Google.Apis.QPXExpress.v1.Data;
 using Google.Apis.Services;
 using Newtonsoft.Json;
+using System.Xml;
 
 namespace Travelopedia
 {
     public partial class Results : System.Web.UI.Page
     {
-        public string responseString;
+        public string responseString = "";
+        public string responseType;
         protected void Page_Load(object sender, EventArgs e)
         {
             if (!IsPostBack)
             {
-                TripsSearchRequest x = new TripsSearchRequest();
-                x.Request = new TripOptionsRequest();
-                x.Request.Passengers = new PassengerCounts { AdultCount = 1 };
-                x.Request.Slice = new List<SliceInput>();
-                x.Request.Slice.Add(new SliceInput() { Origin = Request.QueryString[0], Destination = Request.QueryString[1], Date = "2017-10-11" });
-                x.Request.Slice.Add(new SliceInput() { Origin = Request.QueryString[1], Destination = Request.QueryString[0], Date = "2017-10-28" });
-
-                using (var client = new HttpClient())
+                var queryParam = Request.QueryString["type"];
+                if (queryParam == "hotel")
                 {
-                    client.BaseAddress = new Uri("http://localhost/Travelopedia-API/api/");
-                    client.DefaultRequestHeaders.Accept.Clear();
-                    client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
-
-                    var response = client.PostAsJsonAsync("flights/allflights", x).Result;
-                    if (response.IsSuccessStatusCode)
+                    hiddenField1.Value = "hotel";
+                }
+                else if (queryParam == "flight")
+                {
+                    TripsSearchRequest x = new TripsSearchRequest();
+                    string type = Session["flighttype"].ToString();
+                    if (type == "roundtrip")
                     {
-                        responseString = response.Content.ReadAsStringAsync().Result;
+
+                        x.Request = new TripOptionsRequest();
+                        x.Request.Passengers = new PassengerCounts { AdultCount = 1 };
+                        x.Request.Slice = new List<SliceInput>();
+                        x.Request.Slice.Add(new SliceInput() { Origin = Request.QueryString[1], Destination = Request.QueryString[2], Date = "2017-10-19" });
+                        x.Request.Slice.Add(new SliceInput() { Origin = Request.QueryString[2], Destination = Request.QueryString[1], Date = "2017-10-28" });
+                        hiddenField1.Value = "flightround";
+                    }
+
+                    else
+                    {
+                        x.Request = new TripOptionsRequest();
+                        x.Request.Passengers = new PassengerCounts { AdultCount = 1 };
+                        x.Request.Slice = new List<SliceInput>();
+                        x.Request.Slice.Add(new SliceInput() { Origin = Request.QueryString[1], Destination = Request.QueryString[2], Date = "2017-10-19" });
+                        hiddenField1.Value = "flightone";
+
+                    }
+                    using (var client = new HttpClient())
+                    {
+                        client.BaseAddress = new Uri("http://localhost/Travelopedia-API/api/");
+                        client.DefaultRequestHeaders.Accept.Clear();
+                        client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                        var response = client.PostAsJsonAsync("flights/allflights", x).Result;
+                        if (response.IsSuccessStatusCode)
+                        {
+                            responseString = response.Content.ReadAsStringAsync().Result;
+
+                        }
+                    }
+
+                    if (!ClientScript.IsClientScriptBlockRegistered("myScript"))
+                    {
+                        ClientScript.RegisterClientScriptBlock(typeof(_Default), "myScript", responseString, true);
+                    }
+                    hiddenField2.Value = responseString;
+
+                }
+                else if (queryParam == "car")
+                {
+                    using (var client = new HttpClient())
+                    {
+                        hiddenField1.Value = "car";
                     }
                 }
-
-                if (!ClientScript.IsClientScriptBlockRegistered("myScript"))
-                {
-                    ClientScript.RegisterClientScriptBlock(typeof(_Default), "myScript", responseString, true);
-                }
-
             }
         }
     }
